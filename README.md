79fdba8234bc7b1751b41754c24c89649301cead
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2019-06-17 07:25:06 +0800

    Initial push

---
+++ README.md
@@ -0,0 +1,5 @@
+# go-channels
+
+Simple Go program which demonstrate `channel` and `routine` in Go Programming language.
+
+__TODO: Add more info__
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	links := []string {
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	fmt.Println(links)
+}
