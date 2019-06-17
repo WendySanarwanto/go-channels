ef4a796250e02e91ee4e894a72cccbda891c3814
Wendy Sanarwanto <wendy.sanarwanto@gmail.com>
2019-06-17 22:36:24 +0800

    Added code to check the links

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import ( 
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	links := []string {
@@ -11,5 +14,17 @@ func main() {
 		"http://amazon.com",
 	}
 
-	fmt.Println(links)
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string) {
+	_, err := http.Get(link)
+	if (err != nil) {
+		fmt.Println(link, "might be down.")
+		return
+	}
+
+	fmt.Println(link, "is up !")
 }
